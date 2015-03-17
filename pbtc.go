7c6fda74e91c9cfe3b69ddce7f6e8143f055d3bf
Max Wolter <max.wolter@nexunity.com>
2015-03-17 14:27:00 +0100

    Implemented basic connectivity
    * Listening for incoming connections on all valid local IPv4 addresses
    * Connecting to all valid IPv4 addresses returned by the DNS seeds

--- pbtc.go
+++ pbtc.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"net"
+	"strconv"
+	"time"
 
 	"github.com/CIRCL/pbtc/domain"
 )
@@ -18,5 +21,69 @@ func main() {
 		log.Println(err)
 	}
 
+	// placeholder to stay compilable
 	_ = server
+
+	// empty list of IPs of nodes
+	var nodes []net.IP
+
+	// list of dns seeds for bootstrapping
+	seeds := []string{
+		"seed.bitcoin.sipa.be",
+		"dnsseed.bluematt.me",
+		"dnsseed.bitcoin.dashjr.org",
+		"seed.bitcoinstats.com",
+		"bitseed.xf2.org"}
+
+	// discover IPs for given dns seeds
+	for _, seed := range seeds {
+		ips, err := net.LookupIP(seed)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		for _, ip := range ips {
+			ip := ip.To4()
+
+			if ip == nil {
+				continue
+			}
+
+			log.Println("Discovered at " + ip.String())
+
+			nodes = append(nodes, ip)
+		}
+	}
+
+	// list of connections
+	var conns []net.Conn
+
+	// connect to all discovered nodes
+	for _, node := range nodes {
+		log.Println("Connecting to " + node.String())
+
+		conn, err := net.DialTimeout("tcp4", node.String()+":8333", 1*time.Second)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		conns = append(conns, conn)
+	}
+
+	log.Println(strconv.Itoa(len(conns)) + " total connections")
+
+	// wait for a bit
+	timer := time.NewTimer(8 * time.Second)
+	<-timer.C
+
+	// close all connections
+	for _, conn := range conns {
+		log.Println("Disconnecting from " + conn.RemoteAddr().String())
+
+		conn.Close()
+	}
+
+	log.Println("Exiting")
 }
